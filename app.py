3d989db936be317cd8ff703593494733573eb8f4
e65461123-png <e65461123@gmail.com>
2026-06-03 15:38:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 import os
 
-app = Flask(__name__, template_folder='.') # أخبرنا Flask أن يبحث عن الملفات في المجلد الحالي
+# نحدد المجلد الحالي كمسار رئيسي للبحث
+app = Flask(__name__, template_folder=os.getcwd())
 
 @app.route('/')
 def home():
+    # سيبحث الآن مباشرة في المجلد الرئيسي بدون الحاجة لمجلد templates
     return render_template('login.html')
 
 if __name__ == '__main__':

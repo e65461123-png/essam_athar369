01689abd485c0ca96104fb9dcae2702ea066dd38
e65461123-png <e65461123@gmail.com>
2026-06-03 15:49:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
-# هنا نخبر Flask أن يبحث عن الملفات في المجلد الحالي مباشرة
+# السطر التالي يخبر Flask أن يبحث عن الملفات في نفس المجلد الذي يوجد فيه app.py
 app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def home():
     return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()

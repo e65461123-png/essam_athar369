7cb59caf0d6cb52e8febe7d3218b8c5a3480939d
e65461123-png <e65461123@gmail.com>
2026-06-05 19:37:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def activate_flow():
     
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    c.execute('UPDATE vault SET balance = balance + ? WHERE id = 1', (final_amount,))
+    c.execute('UPDATE vault SET balance = balance + ? WHERE id = 1', (final_amount,)
     conn.commit()
     conn.close()
     

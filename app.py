135f9489bc9972ce3fc2f6ce995868660e96bdb8
e65461123-png <e65461123@gmail.com>
2026-06-06 13:38:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template, request, session, redirect
+
+app = Flask(__name__)
+app.secret_key = "atheer-platform"
+
+users = {
+    "admin": "1234",
+    "Essam": "369369"
+}
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":

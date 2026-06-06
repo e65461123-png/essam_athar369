39809fc73e43cf680ab70b2409468b7a20506792
e65461123-png <e65461123@gmail.com>
2026-06-06 23:34:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
+from flask import request, jsonify
 
-app = Flask(__name__)
-# هذا السطر يقرأ الرابط الذي وضعناه في إعدادات Render
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
-
-# --- ضع كود الجدول هنا ---
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    balance = db.Column(db.Float, default=0.0)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
-
-# --- وضع كود إنشاء الجدول هنا ---
-@app.route('/initdb')
-def initdb():
-    db.create_all()
-    return "تم إنشاء جداول قاعدة البيانات بنجاح!"
-
-# (وبقية كود الموقع الخاص بك هنا)
+@app.route('/create_wallet', methods=['POST'])
+def create_wallet():
+    data = request.json
+    username = data.get('username')
+    
+    # التحقق إذا كان المستخدم موجوداً
+    if User.query.filter_by(username=username).first():
+        return jsonify({"message": "المستخدم موجود بالفعل!"}), 400
+    
+    # إنشاء محفظة جديدة برصيد افتراضي 0
+    new_user = User(username=username, balance=0.0)
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({"message": f"تم إنشاء محفظة للمستخدم {username} بنجاح!"}), 201

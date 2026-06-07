681975a082aab814f2eb8b894837836e558958a8
e65461123-png <e65461123@gmail.com>
2026-06-07 20:11:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
-from blueprints.routes import main_bp  # استيراد الـ Blueprint
+from blueprints.routes import main_bp
+from models.database import db
 
 app = Flask(__name__)
+
+# إعداد قاعدة بيانات محلية (SQLite)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///aether.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# ربط الـ db بالتطبيق
+db.init_app(app)
 
 # تسجيل الـ Blueprint
 app.register_blueprint(main_bp)

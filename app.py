85517668ab3c7f1970a733a4de4ec619e5014f09
e65461123-png <e65461123@gmail.com>
2026-06-07 19:27:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+
+# تهيئة التطبيق
+app = Flask(__name__)
+
+# استيراد وتجهيز المجلدات الفرعية (Blueprints)
+# سنقوم بربطها لاحقاً بمجرد إنشاء الملفات داخل مجلد blueprints
+# from blueprints.auth import auth_bp
+# app.register_blueprint(auth_bp)
+
+@app.route('/')
+def index():
+    return "تم التوصيل بنجاح: المشروع يعمل الآن!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
 

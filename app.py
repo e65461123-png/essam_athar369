061a4a42f5cdf1d4b1d4255bbb610178c46f5490
e65461123-png <e65461123@gmail.com>
2026-06-07 22:09:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from blueprints.routes import main_bp
+import os
 
 app = Flask(__name__)
+
+# إعداد مفتاح الأمان للجلسات (مهم لنظام تسجيل الدخول)
+app.secret_key = 'aether369_secret_key'
 
 # إعداد قاعدة البيانات
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///aether369.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# تعريف الجداول (الخزنة والأعضاء)
+# تعريف الجداول
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -24,7 +28,7 @@ class AuditLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     action = db.Column(db.String(100), nullable=False)
 
-# إنشاء الجداول عند بدء التشغيل
+# إنشاء الجداول
 with app.app_context():
     db.create_all()
 

2d06a1c9392ae7705d932ad76eb9cff58a6276db
e65461123-png <e65461123@gmail.com>
2026-06-07 20:24:09 +0300

    Update routes.py

--- blueprints/routes.py
+++ blueprints/routes.py
@@ -1,8 +1,7 @@
-
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def home():
-    return "المشروع يعمل عبر Blueprint!"
+    return render_template('index.html')

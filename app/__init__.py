1d5fb05d9d7bf46493497cf36549aab603696d4e
e65461123-png <e65461123@gmail.com>
2026-06-18 01:43:36 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    # استيراد الـ Blueprints من ملفات الـ routes مباشرة
+    from auth.routes import auth_bp
+    from main.routes import main_bp
+
+    # تسجيل الـ Blueprints
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+
+    return app
 

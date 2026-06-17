ac3b4a8caffa50695588fad5cf52c382acc7f90f
e65461123-png <e65461123@gmail.com>
2026-06-18 00:30:54 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dev-key-123'
+    # هذا المسار يضمن أن قاعدة البيانات في مجلد الـ instance
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    db.init_app(app)
+    
+    # ربط الموديولات (Blueprints)
+    from .auth.routes import auth_bp
+    from .main.routes import main_bp
+    
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
+    
+    return app
 

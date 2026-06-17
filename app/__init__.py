a0fd6c39147f8e9549ec169ab85e1c13bcdbba58
e65461123-png <e65461123@gmail.com>
2026-06-18 01:37:57 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,1 @@
-from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
-
-    # استيراد الـ Blueprints من المسار الصحيح
-    from auth.routes import auth_bp
-    from main.routes import main_bp
-
-    # تسجيل الـ Blueprints
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(main_bp)
-
-    return app

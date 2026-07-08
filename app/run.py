352cbaeba3069209ebdd8b63b6b65db7cfe183a1
e65461123-png <e65461123@gmail.com>
2026-07-08 20:51:43 +0300

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,11 +1,3 @@
-from flask import Flask
-import os
+from app import create_app
 
-def create_app():
-    # نحدد المسارات بحيث يبحث في المجلدات التي نقلناها داخل app
-    app = Flask(__name__, 
-                template_folder='templates', 
-                static_folder='static')
-    
-    # هنا يمكنك إضافة إعدادات قاعدة البيانات أو الـ Blueprints لاحقاً
-    return app
+app = create_app()

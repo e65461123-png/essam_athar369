8a3483a9e4a9407ea6eb130a2dce5c9f25f13f0f
e65461123-png <e65461123@gmail.com>
2026-07-08 20:52:38 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,5 @@
 from flask import Flask
-import os
 
 def create_app():
-    # نحدد المسار بدقة بناءً على وجود المجلدات داخل app
-    template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-    static_dir = os.path.join(os.path.dirname(__file__), 'static')
-
-    app = Flask(__name__, 
-                template_folder=template_dir, 
-                static_folder=static_dir)
-
+    app = Flask(__name__, template_folder='templates', static_folder='static')
     return app
